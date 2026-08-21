81a2a8402fb2702018b0e65df7da85d2b3c65912
Afonso <hi@afonsoingles.dev>
2026-08-22 00:54:03 +0100

    user onboarding status

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,18 +1,23 @@
 from pydantic import BaseModel, EmailStr, SecretStr, AwareDatetime, Field, PlainSerializer, ConfigDict, field_serializer
 from typing_extensions import Annotated
-
-from typing import Optional
+from enum import Enum
 import datetime
 import uuid
+
+class UserOnboardingStatus(str, Enum):
+    NOT_STARTED = "not_started" # user needs to upload the file
+    IN_PROGRESS = "in_progress" # system is processing the file
+    COMPLETED = "completed" # onboarding done
 
 class SafeUser(BaseModel):
 
     model_config = ConfigDict(extra="ignore", revalidate_instances="always")
-                              
+                             
     id: uuid.UUID =  Field(default_factory=uuid.uuid4)
     name: str
     email: EmailStr
     email_verified: bool = Field(default=False)
+    onboarding_status: UserOnboardingStatus = Field(default=UserOnboardingStatus.NOT_STARTED)
     active: bool = Field(default=True)
     admin: bool = Field(default=False)
     superadmin: bool = Field(default=False) 
