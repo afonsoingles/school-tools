7ad921e2af4509052ea9a9a2b61c1c3dd070cc5b
Afonso <hi@afonsoingles.dev>
2026-08-27 15:55:31 +0100

    lmfao forgot to remove this

--- backend/main.py
+++ backend/main.py
@@ -76,7 +76,3 @@ async def handle_err(request, err: Exception) -> JSONResponse:
 @app.get("/")
 async def root() -> RedirectResponse:
     return RedirectResponse(url=os.environ.get("BASE_URL", "http://localhost:3000/"), status_code=308)
-
-@app.get("/error")
-async def error() -> float:
-    return 1/0
