061e4d8726c050cf4c2dba711aa9a4ca3a5e1eeb
Afonso <hi@afonsoingles.dev>
2026-08-29 19:01:25 +0100

    admin route to force generate feeds

--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from decorators.auth import require_auth
 from utils.database import Database
+from jobs.generate_ics import generate_pending_feeds
+from main import scheduler
 
 router = APIRouter()
 db = Database()
@@ -17,3 +19,10 @@ async def clear_user_cache(request: Request) -> JSONResponse:
         db.redis.delete(*keys)
 
     return JSONResponse({"success": True, "message": "cleared all user cache"})
+
+@router.post("/v1/admin/force_generate_pending_feeds")
+@require_auth(require_admin=True)
+async def force_generate_pending_feeds(request: Request) -> JSONResponse:
+    scheduler.add_job(generate_pending_feeds, id="calendar.generate_pending_feeds")
+
+    return JSONResponse({"success": True, "message": "done! triggered the job to generate pending feeds."})
