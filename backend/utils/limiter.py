e3d03f23314e47482a2a4e94329a796bc94445c7
Afonso <hi@afonsoingles.dev>
2026-08-31 01:13:42 +0100

    im stupid

--- backend/utils/limiter.py
+++ backend/utils/limiter.py
@@ -5,5 +5,5 @@ import os
 limiter = Limiter(
     key_func=get_remote_address,
     storage_uri=os.environ.get("REDIS_URL", "redis://localhost:6379"),
-    enabled=True if os.environ.get("ENVIRONMENT", "development") == "development" else False
+    enabled=True if os.environ.get("ENVIRONMENT", "development") == "production" else False
 )
