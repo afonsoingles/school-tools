c0bd8594c5aa1688d6c2a35eaeae5744c436c0f5
Afonso <hi@afonsoingles.dev>
2026-09-06 02:16:20 +0100

    fix tailwind warnings

--- frontend/src/components/ui/switch.tsx
+++ frontend/src/components/ui/switch.tsx
@@ -9,14 +9,14 @@ function Switch({ className, ...props }: SwitchPrimitive.Root.Props) {
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors outline-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 data-[checked]:bg-primary data-[unchecked]:bg-input",
+        "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors outline-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 data-checked:bg-primary data-unchecked:bg-input",
         className
       )}
       {...props}
     >
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
-        className="pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg transition-transform data-[checked]:translate-x-4 data-[unchecked]:translate-x-0"
+        className="block w-4 h-4 transition-transform rounded-full shadow-lg pointer-events-none bg-background data-checked:translate-x-4 data-unchecked:translate-x-0"
       />
     </SwitchPrimitive.Root>
   )
